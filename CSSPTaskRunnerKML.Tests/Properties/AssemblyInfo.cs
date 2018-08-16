159716eb27115477b63df2089120b0550d7cff1e
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-08-16 17:12:59 -0300

    Initial commit

---
+++ CSSPTaskRunnerKML.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerKML.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerKML.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c545d27-be93-4270-960c-432fe4d339ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSSPTaskRunnerKMLHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerKMLHelper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerKMLHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9a86fa2-7017-4945-a121-b2fae1a46d70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
